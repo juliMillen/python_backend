71243ef2e11b8e303bff7c5ec0ec4ad6ae944e93
Julian <julimillen@gmail.com>
2024-11-21 20:40:57 -0300

    operaciones get,post,put,delete en mongodb

--- FastAPI/routers/users_db.py
+++ FastAPI/routers/users_db.py
@@ -13,10 +13,10 @@ router = APIRouter(prefix="/userbd",
 
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.users.find())
+    return users_schema(db_client.local.users.find())
 
 
-@router.get("/{id}")  # Path
+@router.get("/{id}")  
 async def user(id: str):
     return search_user("_id", ObjectId(id))
 
@@ -28,7 +28,7 @@ async def user(id: str):
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
-    if type(search_user_by_email("email", user.email)) == User:
+    if type(search_user_by_email(user.email)) == User:
        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
     
@@ -48,7 +48,7 @@ async def user(user: User):
     del user_dict["id"]
 
     try:
-        db_client.users.find_one_and_replace(
+        db_client.local.user.find_one_and_replace(
             {"_id": ObjectId(user.id)}, user_dict)
     except:
         return {"error": "No se ha actualizado el usuario"}
@@ -59,24 +59,25 @@ async def user(user: User):
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def user(id: str):
 
-    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
         return {"error": "No se ha eliminado el usuario"}
 
-#def search_user(field: str, key):
+def search_user(field: str, key):
 
-#    try:
-#        user = db_client.users.find_one({field: key})
-#        return User(**user_schema(user))
-#    except:
-#        return {"error": "No se ha encontrado el usuario"}
+    try:
+       user = db_client.users.find_one({field: key})
+       return User(**user_schema(user))
+    except:
+       return {"error": "No se ha encontrado el usuario"}
     
 
 def search_user_by_email(email: str):
     
     try:
-     user =  user_schema(db_client.local.user.find_one({"email": email}))
-     return User(**user_schema(user))    
+     user =  db_client.local.users.find_one({"email": email})
+     return User(**user_schema(user))
     except:
+        print("error")
         return {"error":"No se ha encontrado el usuario"}
